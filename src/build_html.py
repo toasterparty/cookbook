4d20d65a07df41de939aaeeb5f744fb9c6d323dc
Blake Spangenberg <blake.spangenberg1@gmail.com>
2020-05-19 20:13:21 -0700

    Crude html file building

--- src/build_html.py
+++ src/build_html.py
@@ -1,8 +1,55 @@
 import sys
 import json
 
-HEADER
-FOOTER
+# (title,title,image,cook_time,servings,calories_per_serving,ingredients,directions,references,tags)
+HTML_FORMAT = '''
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>%s</title>
+		<style type="text/css">body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>
+	</head>
+	<body>
+		<h1>%s</h1>
+		<img style="width:auto; height:350px;" src="%s">
+		<p><em>Cook Time: %s</em></p>
+		
+		<h2>Seriving Information</h2>
+		<ul>
+			<li>Servings - %s</li>
+			<li>Calories per Serving - %s</li>
+		</ul>
+		
+		<h2>Ingredients</h2>
+		<ul>
+			%s
+		</ul>
+		
+		<h2>Directions</h2>
+		<p>%s</p>
+		
+		<h2>References</h2>
+		<p>
+			<ul>
+				%s
+			</ul>
+		</p>
+
+		<h2>Tags</h2>
+		<p>
+			%s
+		</p>
+
+		<br><hr>
+		
+		<p>
+			<a href="recipes.html">all recipes</a>&ensp;
+			<a href="tags.html">all tags</a>&ensp;
+		</p>
+	</body>
+</html>
+'''
 
 def main():
     try:
@@ -40,6 +87,7 @@ def build_html(input_filename, output_filename):
 
     try:
         title     = input_json['title']
+        image     = input_json['image']
         cook_time = input_json['cook_time']
         servings  = input_json['servings']
         calories_per_serving = input_json['calories_per_serving']
@@ -47,6 +95,10 @@ def build_html(input_filename, output_filename):
         directions = parse_directions(input_json['directions'])
         references = parse_references(input_json['references'])
         tags = parse_tags(input_json['tags'])
+        
+        recipe_html = HTML_FORMAT % (title,title,image,cook_time,servings,calories_per_serving,ingredients,directions,references,tags)
+
+        output_file.write(recipe_html)
 
         print("Successfully built " + output_filename)
     except Exception:
